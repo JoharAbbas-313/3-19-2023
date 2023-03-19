164312d60659da55afa14069c5077fdf66325f85
JoharAbbas-313 <123437515+JoharAbbas-313@users.noreply.github.com>
2023-03-19 09:58:37 -0700

    Add files via upload

---
+++ JawaScript/app.js
@@ -0,0 +1,101 @@
+// var headuser = prompt("enter head username");
+// var tailsuser = prompt("enter tails username");
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+
+// if(floor === 0){
+//   alert("Head " + headuser + " win the toss")
+// }else{
+//   alert("Tails " + tailsuser + " win the toss")
+// }
+
+
+// ================= chp # 28 ======================
+
+// CONVERTING STRING TO INTEGERS AND DECIMALS //
+
+// var msg = 27;
+// var change = msg.parseInt
+//  console.log(change);
+
+
+// convert into string 
+
+// var num = 27;
+// // console.log(num.toString());
+
+// // // convert into number
+
+//  var num = "27";
+//  console.log(num.toNumber())
+
+// var myInteger = parseInt("1.9999");
+
+// console.log(myInteger)
+
+// =========== chp # 30 ============
+
+// var num = 10.54545454;
+// var cont = num.toFixed(2)
+// console.log(cont);
+
+
+// var num = 5.737738;
+// var cont = num.toFixed(1);
+// console.log(cont);
+
+// var num = 2.6836329;
+// var cont = num.toFixed(3);
+// console.log(cont);
+
+
+// var firstName = prompt("enter firstname");
+// var lastName = prompt("enter lastname")
+
+// var fullname = firstName + " " + lastName;
+// document.write(fullname);
+
+
+// var str = "pakistan";
+// document.write(str.indexOf("n"));
+ 
+
+// var str = "Hello World";
+// document.write(str.lastIndexOf("l"));
+
+
+// var str = "Pakistan";
+// document.write(str.charAt("3"));
+
+
+// var city = "Hyderabad";
+//  document.write(city.replace("Hyder","islam"));
+
+
+// var msg = "Ali and Sami are best friends. They play cricket and football togethr";
+// document.write(msg.replaceAll("and","&"))
+
+
+// var num = 472;
+// document.write(num.toString());
+
+
+// var input = "peanuts";
+// input = input.toUpperCase();
+// document.write(input);
+
+
+// var input = "JAWASCRIPT";
+//  input = input.toLowerCase();
+//  document.write(input);
+
+
+// var str = "University of Karachi";
+// arr = [];
+// var word = str.split(" ");
+// console.log(word);
+
+
+// var input = "Pakistan";
+// document.write(input.charAt(7));
